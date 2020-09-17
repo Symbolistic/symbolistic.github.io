3034450fbcdf9fb97a567022496f4add0eca0d8f
Symbol <Symbolistic@live.com>
2020-09-17 03:12:52 -0400

    finished updating

--- projects.js
+++ projects.js
@@ -1,93 +1,92 @@
-const projects = document.querySelectorAll(".project");
+const projects = document.querySelectorAll('.project');
 
 const showAll = () => {
-    projects.forEach((project, index) => {
-        project.classList.remove("hide");
+	projects.forEach((project, index) => {
+		project.classList.remove('hide');
 
-        // This adds the nice gray background to each odd element and keeps the even one index elements white background
-        if (index % 2 === 0) {
-            project.classList.add("gray-bg");
-        } else {
-            project.classList.remove("gray-bg");
-        }
-    });
-}
+		// This adds the nice gray background to each odd element and keeps the even one index elements white background
+		if (index % 2 === 0) {
+			project.classList.add('gray-bg');
+		} else {
+			project.classList.remove('gray-bg');
+		}
+	});
+};
 
-const vanillaJS = () => {
-    projects.forEach(project => {
-        project.classList.add("hide");
-        project.classList.remove("gray-bg");
-    });
+const typescript = () => {
+	projects.forEach((project) => {
+		project.classList.add('hide');
+		project.classList.remove('gray-bg');
+	});
 
-    vanillaJSProjects = document.querySelectorAll(".project.vanillaJS")
-    vanillaJSProjects.forEach((project, index) => {
-        project.classList.remove("hide");
+	typescriptProjects = document.querySelectorAll('.project.typescript');
+	typescriptProjects.forEach((project, index) => {
+		project.classList.remove('hide');
 
-        // This adds the nice gray background to each odd element and keeps the even one index elements white background
-        if (index % 2 === 0) {
-            project.classList.add("gray-bg");
-        } else {
-            project.classList.remove("gray-bg");
-        }
-    });
-}
+		// This adds the nice gray background to each odd element and keeps the even one index elements white background
+		if (index % 2 === 0) {
+			project.classList.add('gray-bg');
+		} else {
+			project.classList.remove('gray-bg');
+		}
+	});
+};
 
 const react = () => {
-    projects.forEach(project => {
-        project.classList.add("hide");
-        project.classList.remove("gray-bg");
-    });
+	projects.forEach((project) => {
+		project.classList.add('hide');
+		project.classList.remove('gray-bg');
+	});
 
-    reactProjects = document.querySelectorAll(".react")
-    reactProjects.forEach((project, index) => {
-        project.classList.remove("hide");
+	reactProjects = document.querySelectorAll('.react');
+	reactProjects.forEach((project, index) => {
+		project.classList.remove('hide');
 
-        // This adds the nice gray background to each odd element and keeps the even one index elements white background
-        if (index % 2 === 0) {
-            project.classList.add("gray-bg");
-        } else {
-            project.classList.remove("gray-bg");
-        }
-    });
-}
+		// This adds the nice gray background to each odd element and keeps the even one index elements white background
+		if (index % 2 === 0) {
+			project.classList.add('gray-bg');
+		} else {
+			project.classList.remove('gray-bg');
+		}
+	});
+};
 
 const fullStack = () => {
-    projects.forEach(project => {
-        project.classList.add("hide");
-        project.classList.remove("gray-bg");
-    });
+	projects.forEach((project) => {
+		project.classList.add('hide');
+		project.classList.remove('gray-bg');
+	});
 
-    fullStackProjects = document.querySelectorAll(".fullstack")
-    fullStackProjects.forEach((project, index) => {
-        project.classList.remove("hide");
+	fullStackProjects = document.querySelectorAll('.fullstack');
+	fullStackProjects.forEach((project, index) => {
+		project.classList.remove('hide');
 
-        // This adds the nice gray background to each odd element and keeps the even one index elements white background
-        if (index % 2 === 0) {
-            project.classList.add("gray-bg");
-        } else {
-            project.classList.remove("gray-bg");
-        }
-    });
-}
+		// This adds the nice gray background to each odd element and keeps the even one index elements white background
+		if (index % 2 === 0) {
+			project.classList.add('gray-bg');
+		} else {
+			project.classList.remove('gray-bg');
+		}
+	});
+};
 
 const d3 = () => {
-    projects.forEach(project => {
-        project.classList.add("hide");
-        project.classList.remove("gray-bg");
-    });
+	projects.forEach((project) => {
+		project.classList.add('hide');
+		project.classList.remove('gray-bg');
+	});
 
-    d3Projects = document.querySelectorAll(".d3")
-    d3Projects.forEach((project, index) => {
-        project.classList.remove("hide");
+	d3Projects = document.querySelectorAll('.d3');
+	d3Projects.forEach((project, index) => {
+		project.classList.remove('hide');
 
-        // This adds the nice gray background to each odd element and keeps the even one index elements white background
-        if (index % 2 === 0) {
-            project.classList.add("gray-bg");
-        } else {
-            project.classList.remove("gray-bg");
-        }
-    });
-}
-
+		// This adds the nice gray background to each odd element and keeps the even one index elements white background
+		if (index % 2 === 0) {
+			project.classList.add('gray-bg');
+		} else {
+			project.classList.remove('gray-bg');
+		}
+	});
+};
 
 console.log(projects);
